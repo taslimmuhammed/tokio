848d761dcfb80c0d86fc96c9f3a77efaaff879f3
taslimmuhammed <taslimmuhammed67@gmail.com>
2024-10-17 07:37:27 +0530

    testing join

--- src/eventuals_t/join.rs
+++ src/eventuals_t/join.rs
@@ -1,6 +1,6 @@
 use std::{borrow::Borrow, time::Duration};
 
-use eventuals::{Eventual, EventualExt};
+use eventuals::{join, Eventual, EventualExt};
 use tokio::{select, sync::watch, time::sleep};
 
 
@@ -8,16 +8,44 @@ use tokio::{select, sync::watch, time::sleep};
 
 pub async fn join_fn() {
     let (mut writer, reader) = Eventual::<i32>::new();
-    writer.write(1);
+    let (mut writer2, reader2) = Eventual::<i32>::new();
     let j1 =  tokio::spawn(async move{
-        println!("writing....");
-        sleep(Duration::from_millis(1000)).await;
+        println!("writing....1");
+        sleep(Duration::from_millis(10000)).await;
         writer.write(3);
-        println!("writing complete");
+        println!("writing complete  1");
         println!("writing....");
-        sleep(Duration::from_millis(1000)).await;
+        sleep(Duration::from_millis(10000)).await;
         writer.write(6);
-        println!("writing complete");
+        println!("writing complete 1");
+        sleep(Duration::from_millis(10000)).await;
+        writer.write(9);
+        println!("writing complete 1");
+        sleep(Duration::from_millis(10000)).await;
+    });
+    let j2 =  tokio::spawn(async move{
+        println!("writing.... 2");
+        sleep(Duration::from_millis(10000)).await;
+        writer2.write(2);
+        println!("writing complete 2");
+        println!("writing.... 2");
+        // sleep(Duration::from_millis(10000)).await;
+        // writer2.write(4);
+        // println!("writing complete 2");
+        // sleep(Duration::from_millis(10000)).await;
+        // writer2.write(8);
+        // println!("writing complete 2");
+    });
+    let eve = join((reader, reader2)).map(move |(r1, r2)| {
+        async  move{
+            println!("joining...");
+            (r1, r2)
+        }
+    });
+    j1.await;
+    j2.await;
+    let _ = eve.pipe(move |x| {
+        println!("{:?}",x);
     });
     // let j2 = tokio::spawn(async move{
     //     loop{
@@ -31,17 +59,17 @@ pub async fn join_fn() {
     //     }
        
     // });
-    let (sender, mut receiver) = watch::channel(0);
-    let _p1 = reader.pipe(move |x|{
-        let _ = sender.send(x);
-    });
-    loop {
-        select! {
-            Ok(_) = receiver.changed()=>{},
-        }
-        let val = *receiver.borrow();
-        println!("values is {val}");
-        
-    }
+    // let (sender, mut receiver) = watch::channel(0);
+    // let _p1 = reader.pipe(move |x|{
+    //     let _ = sender.send(x);
+    // });
+    // loop {
+    //     select! {
+    //         Ok(_) = receiver.changed()=>{},
+    //     }
+    //     let val = *receiver.borrow();
+    //     println!("values is {val}");
+
+    // }
     // j2.await;
 }
